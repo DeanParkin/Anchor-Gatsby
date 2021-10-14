79c0f3872cf80d5d2957cbd02e034bfc82eceb41
Dean Parkin <51768010+DeanParkin@users.noreply.github.com>
2021-10-14 03:16:03 +0100

    yeahh buddy

--- src/pages/index.js
+++ src/pages/index.js
@@ -4,6 +4,7 @@ import Layout from "../components/Layout"
 export default function index() {
   return (
     <div>
+      <p>We gone and fucked it!</p>
       <Layout pageTitle="Yeahhh Buddy!">
         <h2>Yeahhhh Buddy!</h2>
         <p>We did it!</p>
