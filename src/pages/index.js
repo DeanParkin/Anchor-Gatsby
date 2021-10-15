384611d717636ef25c08f156d4238757ac5145ee
Dean Parkin <51768010+DeanParkin@users.noreply.github.com>
2021-10-15 02:15:25 +0100

    text change

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,7 +7,7 @@ export default function index() {
       <p>We gone and fucked it!</p>
       <Layout pageTitle="Yeahhh Buddy!">
         <h2>Yeahhhh Buddy!</h2>
-        <p>We did it!</p>
+        <p>I'm not trying to hack you!</p>
       </Layout>     
     </div>
   )
