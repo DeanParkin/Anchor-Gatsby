58bb1410904d949dc92162a7edba38b45d0fd62c
Dean Parkin <51768010+DeanParkin@users.noreply.github.com>
2021-10-15 02:43:25 +0100

    syntax fix

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import * as React from "react";
-import Layout from "../components/Layout"
+import Layout from "../components/Layout";
+import Test from "../components/Test";
 
 export default function index() {
   return (
