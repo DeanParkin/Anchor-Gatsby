038dccc895a808e8f9386375a955dfcbef803158
Dean Parkin <51768010+DeanParkin@users.noreply.github.com>
2021-10-20 02:47:06 +0100

    next try

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,33 +1,66 @@
-import React from 'react'
-import "../style/anchorNav.css"
-import AnchorNavLogo from "../imges/anchor-nav-logo.png"
-
+import React from "react";
+import AnchorNavLogo from "../images/anchor-nav-logo.png";
 
 export default function Nav() {
-    return (
-        <div>
-            <nav id="navbarSite" role="navigation" className="navbar navbar-expand-lg fixed-top shadow">
-                <a className="navbar-brand" href=".">
-                    <h1>
-                        <span className="hide" hidden="">The Anchor</span>
-                    </h1>
-                    <img src={AnchorNavLogo} alt="The Anchor Logo Home"/>
-                </a>{/*Nav Brand*/}
-                <button className="navbar-toggler navbar-light" id="hamburger" type="button" data-toggle="collapse" data-target="#myToggleNav" aria-controls="myToggleNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <div className="nav-icon">
-                        <div></div>
-                    </div>
-                </button>{/*Hamburger Menu Button*/}
-                <div className="collapse navbar-collapse" id="myToggleNav">
-                    <ul className="navbar-nav justify-content-center">
-                        <li className="nav-item"><a href="#pageTop" className="nav-link">HOME</a></li>
-                        <li className="nav-item"><a href="#covidSection" className="nav-link">COVID-19</a></li>
-                        <li className="nav-item"><a href="#beerSection" className="nav-link">DRINKS</a></li>
-                        <li className="nav-item"><a href="#gallerySection" className="nav-link">GALLERY</a></li>
-                        <li className="nav-item"><a href="#contactForm" className="nav-link ">CONTACT</a></li>
-                    </ul>{/*Nav Menu*/}
-                </div>{/*Collapse Menu*/}
-            </nav>
+  return (
+    <div>
+      <nav
+        id="navbarSite"
+        role="navigation"
+        className="navbar navbar-expand-lg fixed-top shadow"
+      >
+        <a className="navbar-brand" href="#">
+          <h1>
+            <span className="hide" hidden="">
+              The Anchor
+            </span>
+          </h1>
+          <img src={AnchorNavLogo} alt="The Anchor Logo Home"></img>
+        </a>
+        <button
+          className="navbar-toggler navbar-light"
+          id="hamburger"
+          type="button"
+          data-toggle="collapse"
+          data-target="#myToggleNav"
+          aria-controls="myToggleNav"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <div className="nav-icon">
+            <div></div>
+          </div>
+        </button>
+        <div className="collapse navbar-collapse" id="myToggleNav">
+          <ul className="navbar-nav justify-content-center">
+            <li className="nav-item">
+              <a href="#pageTop" className="nav-link">
+                HOME
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#covidSection" className="nav-link">
+                COVID-19
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#beerSection" className="nav-link">
+                DRINKS
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#gallerySection" className="nav-link">
+                GALLERY
+              </a>
+            </li>
+            <li className="nav-item">
+              <a href="#contactForm" className="nav-link ">
+                CONTACT
+              </a>
+            </li>
+          </ul>
         </div>
-    )
+      </nav>
+    </div>
+  );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,15 @@
 import * as React from "react"; 
-import Layout from "../components/Layout";
-import Katie from "../images/Katie.jpg";
-import Test from "../components/Test";
+import * as bootstrap from 'bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 import Nav from "../components/Nav";
+
+
 
 export default function index() {
   return (
     <div>
       <Nav />
-      <Layout pageTitle="Yeahhh Buddy!">
-        <h2>Katie Fucking Rocks!</h2>
-        <p>I'm not trying to hack you!</p>
-        <p>Lets get the Cocky T's going!</p>
-        <Test />
-        <img src={Katie} alt="Katie"></img>
-      </Layout>
+      <h1>yeah buddy!</h1>
     </div>
   )
 }
