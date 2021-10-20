b6c896f382700044ac2a005de5d5b6e3752f5779
Dean Parkin <51768010+DeanParkin@users.noreply.github.com>
2021-10-20 01:01:36 +0100

    class fix

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -4,25 +4,25 @@ import AnchorNavLogo from "../imges/anchor-nav-logo.png"
 export default function Nav() {
     return (
         <div>
-            <nav id="navbarSite" role="navigation" class="navbar navbar-expand-lg fixed-top shadow">
-                <a class="navbar-brand" href=".">
+            <nav id="navbarSite" role="navigation" className="navbar navbar-expand-lg fixed-top shadow">
+                <a className="navbar-brand" href=".">
                     <h1>
-                        <span class="hide" hidden="">The Anchor</span>
+                        <span className="hide" hidden="">The Anchor</span>
                     </h1>
-                    <img src={AnchorNavLogo} alt="The Anchor Logo Home" class="" style=""/>
+                    <img src={AnchorNavLogo} alt="The Anchor Logo Home" className="" style=""/>
                 </a>{/*Nav Brand*/}
-                <button class="navbar-toggler navbar-light" id="hamburger" type="button" data-toggle="collapse" data-target="#myToggleNav" aria-controls="myToggleNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <div class="nav-icon">
+                <button className="navbar-toggler navbar-light" id="hamburger" type="button" data-toggle="collapse" data-target="#myToggleNav" aria-controls="myToggleNav" aria-expanded="false" aria-label="Toggle navigation">
+                    <div className="nav-icon">
                         <div></div>
                     </div>
                 </button>{/*Hamburger Menu Button*/}
-                <div class="collapse navbar-collapse" id="myToggleNav">
-                    <ul class="navbar-nav justify-content-center">
-                        <li class="nav-item"><a href="#pageTop" class="nav-link">HOME</a></li>
-                        <li class="nav-item"><a href="#covidSection" class="nav-link">COVID-19</a></li>
-                        <li class="nav-item"><a href="#beerSection" class="nav-link">DRINKS</a></li>
-                        <li class="nav-item"><a href="#gallerySection" class="nav-link">GALLERY</a></li>
-                        <li class="nav-item"><a href="#contactForm" class="nav-link ">CONTACT</a></li>
+                <div className="collapse navbar-collapse" id="myToggleNav">
+                    <ul className="navbar-nav justify-content-center">
+                        <li className="nav-item"><a href="#pageTop" className="nav-link">HOME</a></li>
+                        <li className="nav-item"><a href="#covidSection" className="nav-link">COVID-19</a></li>
+                        <li className="nav-item"><a href="#beerSection" className="nav-link">DRINKS</a></li>
+                        <li className="nav-item"><a href="#gallerySection" className="nav-link">GALLERY</a></li>
+                        <li className="nav-item"><a href="#contactForm" className="nav-link ">CONTACT</a></li>
                     </ul>{/*Nav Menu*/}
                 </div>{/*Collapse Menu*/}
             </nav>
