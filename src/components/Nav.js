86d9a98d086deea621c37acde2adfe8b48579c99
Dean Parkin <51768010+DeanParkin@users.noreply.github.com>
2021-10-21 03:29:50 +0100

    container

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -3,41 +3,49 @@ import { Nav, Navbar, Container, Offcanvas } from "react-bootstrap";
 import AnchorNavLogo from "../images/anchor-nav-logo.png";
 
 export default function NavBarAnchor() {
-  return (
-    <>
-      <Navbar as="nav" bg="dark" variant="dark" fixed="top" expand={false}>
-        <Container>
-          <Navbar.Brand href="#home">
-            <img
-              alt=""
-              src={AnchorNavLogo}
-              width="30"
-              height="30"
-              className="d-inline-block align-top"
-            />
-            Yeahhh Buddy!
-          </Navbar.Brand>
-          <Navbar.Toggle aria-controls="offcanvasNavbar" />
-    <Navbar.Offcanvas
-      id="offcanvasNavbar"
-      aria-labelledby="offcanvasNavbarLabel"
-      placement="start"
-    >
-      <Offcanvas.Header closeButton>
-        <Offcanvas.Title id="offcanvasNavbarLabel">Deez nutz!!!</Offcanvas.Title>
-      </Offcanvas.Header>
-      <Offcanvas.Body>
-        <Nav className="justify-content-end flex-grow-1 pe-3">
-          <Nav.Link href="#action1">Home</Nav.Link>
-          <Nav.Link href="#action2">Link</Nav.Link>
-          <Nav.Link href="#action2">Yeahhhh</Nav.Link>
-          <Nav.Link href="#action2">Buddy!</Nav.Link>
-          <Nav.Link href="#action2">Its starting to work!!!!</Nav.Link>
-        </Nav>
-      </Offcanvas.Body>
-    </Navbar.Offcanvas>
-        </Container>
-      </Navbar>
-    </>
-  );
+    return (
+        <>
+            <Navbar 
+                as="nav" 
+                bg="dark" 
+                variant="dark" 
+                fixed="top" 
+                expand={false}
+            >
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img
+                            alt=""
+                            src={AnchorNavLogo}
+                            width="30"
+                            height="30"
+                            className="d-inline-block align-top"
+                        />
+                        Yeahhh Buddy!
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="offcanvasNavbar" />
+                    <Navbar.Offcanvas
+                        id="offcanvasNavbar"
+                        aria-labelledby="offcanvasNavbarLabel"
+                        placement="start"
+                    >
+                        <Offcanvas.Header closeButton>
+                            <Offcanvas.Title 
+                                id="offcanvasNavbarLabel"
+                            >Deez nutz!!!</Offcanvas.Title>
+                        </Offcanvas.Header>
+                        <Offcanvas.Body>
+                            <Nav className="justify-content-end flex-grow-1 pe-3">
+                            <Nav.Link href="#action1">Home</Nav.Link>
+                            <Nav.Link href="#action2">Link</Nav.Link>
+                            <Nav.Link href="#action2">Yeahhhh</Nav.Link>
+                            <Nav.Link href="#action2">Buddy!</Nav.Link>
+                            <Nav.Link href="#action2">Its starting to work!!!!</Nav.Link>
+                            </Nav>
+                        </Offcanvas.Body>
+                    </Navbar.Offcanvas>
+                </Container>
+            </Navbar>
+        </>
+    );
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,13 +2,16 @@ import * as React from "react";
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavBarAnchor from "../components/Nav";
 import "../style/style.css"
+import { Container } from "react-bootstrap";
 
 export default function index() {
   return (
     <div>
       <title>Yeah Buddy!</title>
       <NavBarAnchor />
-      <h2>Finally Got it Working!!!</h2>
+      <Container>
+        <h2>Finally Got it Working!!!</h2>
+      </Container>
     </div>
   );
 }
