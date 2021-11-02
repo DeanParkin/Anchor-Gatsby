16a4c65947a3c5202ae9a2772adbc2cded5dadf2
Dean Parkin <51768010+DeanParkin@users.noreply.github.com>
2021-11-02 10:31:39 +0000

    csp

--- gatsby-config.js
+++ gatsby-config.js
@@ -15,7 +15,7 @@ module.exports = {
         mergeDefaultDirectives: false,
         directives: {
           "script-src": "'self' 'unsafe-inline'",
-          "style-src": "'self'",
+          "style-src": "'unsafe-inline'",
           "object-src": "'none'",
           // you can add your directives or override defaults
         },
