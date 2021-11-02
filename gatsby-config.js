83efd68785321d6e018cc2b89075e434b227a184
Dean Parkin <51768010+DeanParkin@users.noreply.github.com>
2021-11-02 09:18:25 +0000

    csp

--- gatsby-config.js
+++ gatsby-config.js
@@ -12,11 +12,11 @@ module.exports = {
         reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
         mergeScriptHashes: true, // you can disable scripts sha256 hashes
         mergeStyleHashes: true, // you can disable styles sha256 hashes
-        mergeDefaultDirectives: true,
+        mergeDefaultDirectives: false,
         directives: {
           "script-src": "'self'",
           "style-src": "'self' 'unsafe-inline'",
-          "img-src": "'self'",
+          "img-src": "*",
           // you can add your directives or override defaults
         },
       },
