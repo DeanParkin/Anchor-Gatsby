ccd0d47c087187c0dfaad0475c34e0d0c7f54e41
Dean Parkin <51768010+DeanParkin@users.noreply.github.com>
2021-11-02 09:35:12 +0000

    csp

--- gatsby-config.js
+++ gatsby-config.js
@@ -14,7 +14,7 @@ module.exports = {
         mergeStyleHashes: true, // you can disable styles sha256 hashes
         mergeDefaultDirectives: false,
         directives: {
-          "script-src": "'self' 'unsafe-inline",
+          "script-src": "'self' 'unsafe-inline'",
           "style-src": "'self'",
           "img-src": "*",
           "object-src": "'none'",
