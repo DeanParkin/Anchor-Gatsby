1b8a4e6b89b5491f796e441c70cf3cbe5afd53f6
Dean Parkin <51768010+DeanParkin@users.noreply.github.com>
2021-11-03 02:08:30 +0000

    csp header

--- gatsby-config.js
+++ gatsby-config.js
@@ -25,9 +25,9 @@ module.exports = {
       resolve: ` gatsby-plugin-netlify-headers`,
       options: {
         headers: {
-          "/*": [
+          "/*":
             "Content-Security-Policy: script-src 'self' 'unsafe-inline'; style-src 'unsafe-inline'; object-src 'none';",
-          ],
+          ,
         },
       },
     },
