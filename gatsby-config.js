37244634465bdf2eb4695a37751a16211281e4d2
Dean Parkin <51768010+DeanParkin@users.noreply.github.com>
2021-11-03 05:20:14 +0000

    csp remove

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,22 +5,22 @@ module.exports = {
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
-    {
-      resolve: `gatsby-plugin-csp`,
-      options: {
-        disableOnDev: true,
-        reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
-        mergeScriptHashes: true, // you can disable scripts sha256 hashes
-        mergeStyleHashes: false, // you can disable styles sha256 hashes
-        mergeDefaultDirectives: false,
-        directives: {
-          "script-src": "'self'",
-          "style-src": "'self'",
-          "object-src": "'none'",
-          // you can add your directives or override defaults
-        },
-      },
-    },
+    // {
+    //   resolve: `gatsby-plugin-csp`,
+    //   options: {
+    //     disableOnDev: true,
+    //     reportOnly: false, // Changes header to Content-Security-Policy-Report-Only for csp testing purposes
+    //     mergeScriptHashes: true, // you can disable scripts sha256 hashes
+    //     mergeStyleHashes: false, // you can disable styles sha256 hashes
+    //     mergeDefaultDirectives: false,
+    //     directives: {
+    //       "script-src": "'self'",
+    //       "style-src": "'self'",
+    //       "object-src": "'none'",
+    //       // you can add your directives or override defaults
+    //     },
+    //   },
+    // },
     `gatsby-plugin-offline`,
     {
       resolve: `gatsby-plugin-manifest`,
